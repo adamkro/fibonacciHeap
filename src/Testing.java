cfccc870f9da146fd6400eb221d8d2f5593bea87
Tomer <tomertzob@gmail.com>
2020-12-21 21:24:23 +0200

    commit all

--- src/Testing.java
+++ src/Testing.java
@@ -2,15 +2,34 @@ public class Testing {
 
     public static void manualTest(){
         FibonacciHeap h = new FibonacciHeap();
-        for (int i = 0; i < 5; i++) {
-            h.insert(i);
-        }
-        h.displayRoots();
-        printHeap.printHeapFib(h);
-        System.out.println(h.getRoot().getKey());
+        h.insert(5);
+        h.insert(3);
+        h.insert(4);
+        h.insert(2);
+        h.insert(0);
+        h.insert(1);
+//        for (int i = 0; i < 5; i++) {
+//            h.insert(i);
+//        }
+//        h.displayRoots();
+//        printHeap.printHeapFib(h);
+//        System.out.println(h.getRoot().getKey());
+//        h.deleteMin();
+//        printHeap.printHeapFib(h);
+//        h.displayRoots();
         h.deleteMin();
-        printHeap.printHeapFib(h);
-        h.displayRoots();
+        System.out.println(h.findMin().getKey());
+        h.deleteMin();
+        System.out.println(h.findMin().getKey());
+        h.deleteMin();
+        System.out.println(h.findMin().getKey());
+        h.deleteMin();
+        System.out.println(h.findMin().getKey());
+        h.deleteMin();
+        System.out.println(h.findMin().getKey());
+        h.deleteMin();
+        System.out.println(h.findMin());
+
     }
 
     public static void main(String[] args){
